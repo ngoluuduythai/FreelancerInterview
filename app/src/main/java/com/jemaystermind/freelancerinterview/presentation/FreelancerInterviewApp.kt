2be5731d887c12c1280815c763f8c0e1c6dc39f3
Jeremy Tecson <jemay.mastermind@gmail.com>
2017-09-17 12:10:59 +0800

    Add copyright

---
+++ app/src/main/java/com/jemaystermind/freelancerinterview/presentation/FreelancerInterviewApp.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2017 Jeremy Tecson
+ *
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ */
+
+package com.jemaystermind.freelancerinterview.presentation
+
+import android.app.Application
+
+class FreelancerInterviewApp : Application() {
+
+  override fun onCreate() {
+    super.onCreate()
+  }
+}
