0172b5f042a2179e88e6e3b7686c154ac3d2af12
Jeremy Tecson <jemay.mastermind@gmail.com>
2017-09-22 01:44:13 +0800

    Add empty state on the review tab
    The image used is not mine. Credits to Jordi Tambillo.
    See link: https://dribbble.com/shots/2478890-Empty-state-Illustration

--- app/src/main/java/com/jemaystermind/freelancerinterview/ui/profile/review/ReviewController.kt
+++ app/src/main/java/com/jemaystermind/freelancerinterview/ui/profile/review/ReviewController.kt
@@ -8,14 +8,12 @@
 package com.jemaystermind.freelancerinterview.ui.profile.review
 
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.jemaystermind.freelancerinterview.R
 import com.jemaystermind.freelancerinterview.ui.ButterKnifeController
 import com.jemaystermind.freelancerinterview.ui.inflate
-import kotlinx.android.synthetic.main.controller_profile_review.view.recyler
 
 class ReviewController : ButterKnifeController {
 
@@ -30,6 +28,6 @@ class ReviewController : ButterKnifeController {
       container.inflate(R.layout.controller_profile_review)
 
   override fun onViewBound(view: View) {
-    view.recyler.layoutManager = LinearLayoutManager(activity)
+    // Do nothing for now. :(
   }
 }
