24e425b0b86f176ff9592ccb58a7d36f9d8ef7ed
Jeremy Tecson <jemay.mastermind@gmail.com>
2017-09-22 00:34:28 +0800

    Add copyright

--- app/src/main/java/com/jemaystermind/freelancerinterview/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/jemaystermind/freelancerinterview/ui/profile/ProfileViewModel.kt
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2017 Jeremy Tecson
+ *
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ */
+
 package com.jemaystermind.freelancerinterview.ui.profile
 
 import com.jemaystermind.freelancerinterview.ui.profile.details.ExamViewModel
